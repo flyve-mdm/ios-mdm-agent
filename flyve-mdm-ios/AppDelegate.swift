10bd945d00bd3e0f5b60e9170364b6cc6ef223f3
Hector Rondon <hrondon@s42.com.ve>
2017-05-03 14:55:01 -0400

    Remove storyboard

--- flyve-mdm-ios/AppDelegate.swift
+++ flyve-mdm-ios/AppDelegate.swift
@@ -1,10 +1,29 @@
-//
-//  AppDelegate.swift
-//  flyve-mdm-ios
-//
-//  Created by Hector Rondon on 03/05/17.
-//  Copyright © 2017 Teclib. All rights reserved.
-//
+/*
+ *   Copyright © 2017 Teclib. All rights reserved.
+ *
+ * AppDelegate.swift is part of flyve-mdm-ios
+ *
+ * flyve-mdm-ios is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * flyve-mdm-ios is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * flyve-mdm-ios is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Hector Rondon
+ * @date      03/05/17
+ * @copyright   Copyright © 2017 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-ios
+ * @link      http://www.glpi-project.org/
+ * ------------------------------------------------------------------------------
+ */
 
 import UIKit
 
@@ -15,7 +34,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = ViewController()
+        
         return true
     }
 
--- flyve-mdm-ios/ViewController.swift
+++ flyve-mdm-ios/ViewController.swift
@@ -1,25 +1,36 @@
-//
-//  ViewController.swift
-//  flyve-mdm-ios
-//
-//  Created by Hector Rondon on 03/05/17.
-//  Copyright © 2017 Teclib. All rights reserved.
-//
+/*
+ *   Copyright © 2017 Teclib. All rights reserved.
+ *
+ * ViewController.swift is part of flyve-mdm-ios
+ *
+ * flyve-mdm-ios is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * flyve-mdm-ios is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * flyve-mdm-ios is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Hector Rondon
+ * @date      03/05/17
+ * @copyright   Copyright © 2017 Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/flyve-mdm-ios
+ * @link      http://www.glpi-project.org/
+ * ------------------------------------------------------------------------------
+ */
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    override func loadView() {
+        super.loadView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
