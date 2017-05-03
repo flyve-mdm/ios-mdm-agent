18423e24fd4adba230d9f7a3454bb0cef800bd23
Hector Rondon <hrondon@s42.com.ve>
2017-05-03 14:46:48 -0400

    Creation xcode project

---
+++ flyve-mdm-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flyve-mdm-ios
+//
+//  Created by Hector Rondon on 03/05/17.
+//  Copyright © 2017 Teclib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
