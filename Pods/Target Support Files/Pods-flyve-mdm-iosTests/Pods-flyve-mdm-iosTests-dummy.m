8acb0903283374c4660a5a3155887d1c124ff803
Gianfranco Manganiello <gianfrancomanganiello@hotmail.com>
2017-08-02 16:16:51 -0400

    Initial commit of gh-pages ref: https://github.com/flyve-mdm/flyve-mdm-ios-agent/issues/71

--- Pods/Target Support Files/Pods-flyve-mdm-iosTests/Pods-flyve-mdm-iosTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flyve_mdm_iosTests : NSObject
-@end
-@implementation PodsDummy_Pods_flyve_mdm_iosTests
-@end
--- Pods/Target Support Files/Pods-flyve-mdm-iosUITests/Pods-flyve-mdm-iosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_flyve_mdm_iosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_flyve_mdm_iosUITests
-@end
