0a79790e7c1e8dbb807d1932b36b9811f9649292
Hector Rondon <hrondon@teclib.com>
2017-08-23 19:19:56 -0400

    ci(release): create tag with conventional-github-releaser

--- scripts/run-build.sh
+++ scripts/run-build.sh
@@ -5,28 +5,28 @@ if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]]; the
 elif [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "true" ]]; then
   fastlane test
 elif [[ "$TRAVIS_BRANCH" == "feature/version" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-
   echo ------------------CONFIG--------------------
-  git config --global user.email $GH_EMAIL
-  git config --global user.name "Travis CI"
-  git remote rm origin
-  git remote add origin https://$GH_USER:${GH_TOKEN}@github.com/flyve-mdm/flyve-mdm-ios-agent.git
+    git config --global user.email $GH_EMAIL
+    git config --global user.name "Travis CI"
+    git remote remove origin
+    git remote add origin https://$GH_USER:$GH_TOKEN@github.com/flyve-mdm/flyve-mdm-ios-agent.git
+    echo ------------------RELEASE--------------------
+    
+    if [[ $TRAVIS_COMMIT_MESSAGE != *"**version**"* && $TRAVIS_COMMIT_MESSAGE != *"**CHANGELOG.md**"* ]]; then
+        git checkout $TRAVIS_BRANCH -f
 
-  if GIT_DIR=$PWD/.git git rev-parse "$1^{tag}" >/dev/null 2>&1 
-  then
-    npm run release
-  else
-    npm run release -- --first-release
-  fi
-  # Get version number from package
-  export GIT_TAG=$(jq -r ".version" package.json)
+        npm run release -- --first-release -m "ci(release): generate **CHANGELOG.md** for version %s"
+        # # Get version number from package
+        export GIT_TAG=$(jq -r ".version" package.json)
+        git push origin ${GIT_TAG}
+        # # Update CFBundleShortVersionString   
+        /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${GIT_TAG}" ${PWD}/${APPNAME}/Info.plist
+        git add ${APPNAME}/Info.plist
+        # Rename last commit
+        git commit -m "ci(build): increment **version** ${GIT_TAG}"
 
-  # Update CFBundleShortVersionString
-  /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${GIT_TAG}" ${PWD}/${APPNAME}/Info.plist
-
-  # Rename last commit
-  git commit -a --amend -m "ci(release): generate CHANGELOG.md for version ${GIT_TAG}"
-
-  git push origin -u feature/version
-  # fastlane release
+        git push origin $TRAVIS_BRANCH
+        
+        # fastlane release
+    fi
 fi
