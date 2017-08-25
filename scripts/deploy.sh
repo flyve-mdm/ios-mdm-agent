f64df91018d39a476a52cb514804f07c3ade5ad1
Hector Rondon <hrondon@teclib.com>
2017-08-25 15:07:12 -0400

    ci(release): execute release just once for tag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,21 +9,23 @@ elif [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; th
     git remote remove origin
     git remote add origin https://$GH_USER:$GH_TOKEN@github.com/flyve-mdm/flyve-mdm-ios-agent.git
 
-    git checkout $TRAVIS_BRANCH -f
-    # Generate CHANGELOG.md and increment version
-    npm run release -- -t '' -m "ci(release): generate **CHANGELOG.md** for version %s"
-    # Push tag to github
-    conventional-github-releaser -t $GH_TOKEN -r 0
-    # Get version number from package.json
-    export GIT_TAG=$(jq -r ".version" package.json)
-    # Update CFBundleShortVersionString
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${GIT_TAG}" ${PWD}/${APPNAME}/Info.plist
-    # Add modified and delete files
-    git add -u
-    # Create commit
-    git commit -m "ci(build): increment **version** ${GIT_TAG}"
-    # Push commits and tags to origin branch
-    git push --follow-tags origin $TRAVIS_BRANCH
-
-    fastlane release
+    if [[ $TRAVIS_COMMIT_MESSAGE != *"**version**"* && $TRAVIS_COMMIT_MESSAGE != *"**CHANGELOG.md**"* ]]; then
+        git checkout $TRAVIS_BRANCH -f
+        # Generate CHANGELOG.md and increment version
+        npm run release -- -t '' -m "ci(release): generate **CHANGELOG.md** for version %s"
+        # Push tag to github
+        conventional-github-releaser -t $GH_TOKEN -r 0
+        # Get version number from package.json
+        export GIT_TAG=$(jq -r ".version" package.json)
+        # Update CFBundleShortVersionString
+        /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${GIT_TAG}" ${PWD}/${APPNAME}/Info.plist
+        # Add modified and delete files
+        git add -u
+        # Create commit
+        git commit -m "ci(build): increment **version** ${GIT_TAG}"
+        # Push commits and tags to origin branch
+        git push --follow-tags origin $TRAVIS_BRANCH
+        
+        fastlane release
+    fi
 fi
