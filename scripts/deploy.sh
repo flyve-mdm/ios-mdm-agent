5b25f160fb5e816a217fdd05f1e45b2296f46704
Hector Rondon <hrondon@teclib.com>
2017-08-25 14:52:02 -0400

    ci(release): configure git in travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,10 @@
 
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   fastlane beta
+
+elif [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    git config --global user.email $GH_EMAIL
+    git config --global user.name "Flyve MDM"
+    git remote remove origin
+    git remote add origin https://$GH_USER:$GH_TOKEN@github.com/flyve-mdm/flyve-mdm-ios-agent.git
 fi
