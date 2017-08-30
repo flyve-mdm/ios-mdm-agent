45ea5fae67a87383b4fbe3383707fa03bc1384f6
Hector Rondon <hrondon@teclib.com>
2017-08-30 11:46:54 -0400

    Revert "ci(release): validate  is not null"
    This reverts commit b5950becf5d94cbdf9b42970a18ebb66838cdcd9.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -25,12 +25,10 @@
 # @link      https://.flyve-mdm.com
 # ------------------------------------------------------------------------------
 
-if [[ -n $GH_TOKEN ]]; then
-    git config --global user.email $GH_EMAIL
-    git config --global user.name "Flyve MDM"
-    git remote remove origin
-    git remote add origin https://$GH_USER:$GH_TOKEN@github.com/flyve-mdm/flyve-mdm-ios-agent.git
-fi
+git config --global user.email $GH_EMAIL
+git config --global user.name "Flyve MDM"
+git remote remove origin
+git remote add origin https://$GH_USER:$GH_TOKEN@github.com/flyve-mdm/flyve-mdm-ios-agent.git
 
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
 
