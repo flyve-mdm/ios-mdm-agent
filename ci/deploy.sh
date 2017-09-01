37ee63d90bc2f005497a39718ed0f7585a2ec080
Hector Rondon <hrondon@teclib.com>
2017-09-01 14:44:18 -0400

    ci(deploy): replace repo naname for TRAVIS_REPO_SLUG

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -29,7 +29,7 @@ if [[ -n $GH_TOKEN ]]; then
     git config --global user.email $GH_EMAIL
     git config --global user.name "Flyve MDM"
     git remote remove origin
-    git remote add origin https://$GH_USER:$GH_TOKEN@github.com/flyve-mdm/flyve-mdm-ios-agent.git
+    git remote add origin https://$GH_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git
 fi
 
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
