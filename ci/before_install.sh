0a07d16fe8c3a4839f79091dd205b777d8e216b0
Hector Rondon <hrondon@teclib.com>
2017-09-07 13:58:40 -0400

    ci(circle): change environment variables from TravisCI to CircleCI

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -30,4 +30,4 @@ echo ----------- Create Fastlane environment variables ------------
 echo FASTLANE_PASSWORD=$FASTLANE_PASSWORD >> .env
 echo TELEGRAM_WEBHOOKS=$TELEGRAM_WEBHOOKS >> .env
 echo GIT_REPO=$TRAVIS_REPO_SLUG >> .env
-echo GIT_BRANCH=$TRAVIS_BRANCH >> .env
+echo GIT_BRANCH=$CIRCLE_BRANCH >> .env
