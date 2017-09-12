2207d1e2d759109419a72b151ae6d9b9779946a6
Hector Rondon <hrondon@teclib.com>
2017-09-12 09:21:02 -0400

    ci(localization): request to transifex only on develop branch

--- ci/before_test.sh
+++ ci/before_test.sh
@@ -27,7 +27,7 @@
 
 echo ------------------- Configure Transifex --------------------
 # Configure Transifex
-if [[ ("$CIRCLE_BRANCH" == "develop" || "$CIRCLE_BRANCH" == "master") && "$CI_PULL_REQUEST" == "" ]]; then
+if [[ "$CIRCLE_BRANCH" == "develop" && "$CI_PULL_REQUEST" == "" ]]; then
     # Create config file transifex
     sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
     
