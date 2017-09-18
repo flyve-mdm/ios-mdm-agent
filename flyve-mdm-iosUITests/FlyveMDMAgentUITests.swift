2260f6be3821926f58286987697441805749b751
Naylin <nmedina@teclib.com>
2017-09-18 18:05:05 -0400

    docs(tearDown): change the description

--- flyve-mdm-iosUITests/FlyveMDMAgentUITests.swift
+++ flyve-mdm-iosUITests/FlyveMDMAgentUITests.swift
@@ -23,8 +23,8 @@ class FlyveMDMAgentUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
+    /// This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
