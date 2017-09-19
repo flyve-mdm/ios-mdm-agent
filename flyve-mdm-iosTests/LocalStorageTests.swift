34066b7b4fad968fb6d77066cad70f26c068c22b
Naylin <nmedina@teclib.com>
2017-09-19 17:29:08 -0400

    docs(tearDown): change the description

--- flyve-mdm-iosTests/LocalStorageTests.swift
+++ flyve-mdm-iosTests/LocalStorageTests.swift
@@ -17,8 +17,8 @@ class LocalStorageTests: XCTestCase {
         super.setUp()
     }
     
+    /// This method is called after the invocation of each test method in the class.
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
