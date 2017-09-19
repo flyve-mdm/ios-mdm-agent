f56e4fd5ba14d90df970e5a153b2665df68af487
Naylin <nmedina@teclib.com>
2017-09-19 17:44:35 -0400

    docs(testBase64Decoded): add the description

--- flyve-mdm-iosTests/StringHelperTests.swift
+++ flyve-mdm-iosTests/StringHelperTests.swift
@@ -27,6 +27,7 @@ class StringHelperTests: XCTestCase {
         XCTAssertEqual("flyve".base64Encoded(), "Zmx5dmU=", "Base64 Encoded not valid")
     }
     
+    /// Test if the base64 decode is valid
     func testBase64Decoded() {
         XCTAssertEqual("Zmx5dmU=".base64Decoded(), "flyve", "Base64 Decoded not valid")
     }
