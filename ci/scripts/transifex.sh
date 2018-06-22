8e49cca7f66cb5c563aa011e7233a939aa4de18c
Hector Rondon <hrondon@teclib.com>
2018-06-22 17:44:54 -0400

    ci(deploy): migrate develop deploy to circleCI 2.0
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- ci/scripts/transifex.sh
+++ ci/scripts/transifex.sh
@@ -29,9 +29,6 @@ echo ------------------- Configure Transifex --------------------
 # Configure Transifex on develop branch
 # Create config file transifex
 sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
-
-# Move to local branch
-git checkout $CIRCLE_BRANCH -f
 # get transifex status
 tx status
 # push local files to transifex
@@ -39,7 +36,7 @@ tx push --source --no-interactive
 # pull all the new language
 tx pull --all --force
 # if there are changes in lenguages
-if [[ -z $(git status -s) ]]; then
+if [[ -z $(git status -uno -s) ]]; then
     echo "tree is clean"
 else
     git add -u
